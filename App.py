66521bbc077d6b7de7c71dc1b12809701cbf948d
aysh2003 <aysharesh29@gmail.com>
2026-03-04 00:13:31 +0530

    Update App.py

--- App.py
+++ App.py
@@ -218,18 +218,18 @@ def run():
 
             st.subheader("**Skills Recommendation💡**")
                 ## Skill shows
-                keywords = st_tags(label='### Skills that you have',
+            keywords = st_tags(label='### Skills that you have',
                                    text='See our skills recommendation',
                                    value=resume_data['skills'], key='1')
 
                 ##  recommendation
-                ds_keyword = ['tensorflow', 'keras', 'pytorch', 'machine learning', 'deep Learning', 'flask',
+            ds_keyword = ['tensorflow', 'keras', 'pytorch', 'machine learning', 'deep Learning', 'flask',
                               'streamlit']
-                web_keyword = ['react', 'django', 'node jS', 'react js', 'php', 'laravel', 'magento', 'wordpress',
+            web_keyword = ['react', 'django', 'node jS', 'react js', 'php', 'laravel', 'magento', 'wordpress',
                                'javascript', 'angular js', 'c#', 'flask']
-                android_keyword = ['android', 'android development', 'flutter', 'kotlin', 'xml', 'kivy']
-                ios_keyword = ['ios', 'ios development', 'swift', 'cocoa', 'cocoa touch', 'xcode']
-                uiux_keyword = ['ux', 'adobe xd', 'figma', 'zeplin', 'balsamiq', 'ui', 'prototyping', 'wireframes',
+            android_keyword = ['android', 'android development', 'flutter', 'kotlin', 'xml', 'kivy']
+            ios_keyword = ['ios', 'ios development', 'swift', 'cocoa', 'cocoa touch', 'xcode']
+            uiux_keyword = ['ux', 'adobe xd', 'figma', 'zeplin', 'balsamiq', 'ui', 'prototyping', 'wireframes',
                                 'storyframes', 'adobe photoshop', 'photoshop', 'editing', 'adobe illustrator',
                                 'illustrator', 'adobe after effects', 'after effects', 'adobe premier pro',
                                 'premier pro', 'adobe indesign', 'indesign', 'wireframe', 'solid', 'grasp',
