e868d85e9bc0209788f5d745b7b5351fd51c6657
aysh2003 <aysharesh29@gmail.com>
2026-03-03 19:47:48 +0530

    Update App.py

--- App.py
+++ App.py
@@ -136,7 +136,21 @@ def run():
             save_path = os.path.join(folder, pdf_file.name)
             with open(save_path, "wb") as f:
                f.write(pdf_file.getbuffer())
-            st.pdf(save_image_path)
+            # After saving the PDF file to save_image_path
+            import base64
+
+            with open(save_image_path, "rb") as pdf_file:
+                base64_pdf = base64.b64encode(pdf_file.read()).decode("utf-8")
+
+            pdf_display = f'''
+               <iframe
+                 src="data:application/pdf;base64,{base64_pdf}"
+                 width="700" height="1000"
+                 type="application/pdf"
+               ></iframe>
+            '''
+
+            st.markdown(pdf_display, unsafe_allow_html=True)
             resume_data = ResumeParser(save_image_path).get_extracted_data()
             if resume_data:
                 ## Get the whole resume data
