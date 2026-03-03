826fcc882acf754187082a13b78f8418680d73f6
aysh2003 <aysharesh29@gmail.com>
2026-03-03 19:32:08 +0530

    Update App.py

--- App.py
+++ App.py
@@ -103,7 +103,7 @@ def insert_data(name, email, res_score, timestamp, no_of_pages, reco_field, cand
 
 st.set_page_config(
     page_title="Smart Resume Analyzer",
-    page_icon='./Logo/SRA_Logo.ico',
+    page_icon='SRA_Logo.ico',
 )
 
 
@@ -114,7 +114,7 @@ def run():
     choice = st.sidebar.selectbox("Choose among the given options:", activities)
     # link = '[©Developed by Spidy20](http://github.com/spidy20)'
     # st.sidebar.markdown(link, unsafe_allow_html=True)
-    img = Image.open('./Logo/SRA_Logo.jpg')
+    img = Image.open('SRA_Logo.jpg')
     img = img.resize((250, 250))
     st.image(img)
 
