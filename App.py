c27d46c0f2a548c65ce5da734e7b7485344211ad
aysh2003 <aysharesh29@gmail.com>
2026-03-04 16:07:02 +0530

    Update App.py

--- App.py
+++ App.py
@@ -485,7 +485,7 @@ def run():
                         connection.commit()
             else:
                              st.error('Something went wrong..')
- else:
+    else:
         ## Admin Side
         st.success('Welcome to Admin Side')
         # st.sidebar.subheader('**ID / Password Required!**')
